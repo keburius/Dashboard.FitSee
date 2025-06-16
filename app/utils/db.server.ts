e9d34ed377f9f611db84c1bcf551a11b8e19a541
giorgikeburia <giorgikeburia@lemondo.com>
2025-06-16 19:17:41 +0400

    fix

--- app/utils/db.server.ts
+++ app/utils/db.server.ts
@@ -3,14 +3,18 @@ import { PrismaClient } from "@prisma/client";
 let prisma: PrismaClient;
 
 declare global {
-  var __prisma__: PrismaClient;
+  var __prisma__: PrismaClient | undefined;
 }
 
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
+  prisma = new PrismaClient({
+    log: ["error", "warn"],
+  });
 } else {
   if (!global.__prisma__) {
-    global.__prisma__ = new PrismaClient();
+    global.__prisma__ = new PrismaClient({
+      log: ["query", "error", "warn"],
+    });
   }
   prisma = global.__prisma__;
 }
